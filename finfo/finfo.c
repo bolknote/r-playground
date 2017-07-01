6b29e7663f6010fde399cea458abc28f42039b55
Evgeny Stepanischev <Evgeny.Stepanischev@tatar.ru>
2017-07-01 20:58:36 +0300

    Convert tabs in spaces

--- finfo/finfo.c
+++ finfo/finfo.c
@@ -6,15 +6,15 @@ static magic_t magic = NULL;
 
 void finfo_open(int *flags, char** magicfile)
 {
-	if (NULL == magic) {
-		magic = magic_open(*flags);
+    if (NULL == magic) {
+        magic = magic_open(*flags);
 
-	    if (NULL == magic) {
-	        error("Unable to initialize the library");
-	    }
-	}
+        if (NULL == magic) {
+            error("Unable to initialize the library");
+        }
+    }
 
-	if (0 != magic_load(magic, magicfile ? *magicfile : NULL)) {
+    if (0 != magic_load(magic, magicfile ? *magicfile : NULL)) {
         magic_close(magic);
         error("Can't load magic database: %s\n", magic_error(magic));
     }
@@ -22,10 +22,10 @@ void finfo_open(int *flags, char** magicfile)
 
 void finfo_buffer(char** buffer, const char** out)
 {
-	*out = strdup(magic_buffer(magic, *buffer, strlen(*buffer)));
+    *out = strdup(magic_buffer(magic, *buffer, strlen(*buffer)));
 }
 
 void finfo_file(char** filename, const char** out)
 {
-	*out = strdup(magic_file(magic, *filename));
+    *out = strdup(magic_file(magic, *filename));
 }
