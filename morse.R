e588d8956936e04935893e0f7ff0e7f3b3ff3a17
Evgeny Stepanischev <Evgeny.Stepanischev@tatar.ru>
2018-02-27 17:03:06 +0300

    Morse code as unary R operators

---
+++ morse.R
@@ -0,0 +1,108 @@
+#!/usr/bin/env Rscript
+library(methods)
+
+init <- function() {
+	code <<- list(
+		'+-'	= 'A',
+		'-+++'	= 'B',
+		'-+-+'	= 'C',
+		'-++'	= 'D',
+		'+'		= 'E',
+		'++-+'	= 'F',
+		'--+'	= 'G',
+		'++++'	= 'H',
+		'++'	= 'I',
+		'+---'	= 'J',
+		'-+-'	= 'K',
+		'+-++'	= 'L',
+		'--'	= 'M',
+		'-+'	= 'N',
+		'---'	= 'O',
+		'+--+'	= 'P',
+		'--+-'	= 'Q',
+		'+-+'	= 'R',
+		'+++'	= 'S',
+		'-'		= 'T',
+		'++-'	= 'U',
+		'+++-'	= 'V',
+		'+--'	= 'W',
+		'-++-'	= 'X',
+		'-+--'	= 'Y',
+		'--++'	= 'Z',
+		'+----'	= '1',
+		'++---'	= '2',
+		'+++--'	= '3',
+		'++++-'	= '4',
+		'+++++'	= '5',
+		'-++++'	= '6',
+		'--+++'	= '7',
+		'---++'	= '8',
+		'----+'	= '9',
+		'-----'	= '0',
+		'++++++'= '.',
+		'+-+-+-'= ',',
+		'---+++'= ':',
+		'-+-+-+'= ';',
+		'+----+'= "'",
+		'+-++-+'= '"',
+		'-++++-'= '—',
+		'-++-+'	= '/',
+		'++--++'= '?',
+		'--++--'= '!',
+		'-+++-'	= ' ',
+		'+--+-+'= '@'
+	)
+
+	setGeneric('toMorseStr', function(str) standardGeneric('toMorseStr'))
+	setMethod('toMorseStr', 'character', function (str) {
+		code.rev <<- setNames(names(code), code)
+		str.vec <<- unlist(strsplit(toupper(str), split=NULL))
+		morse <<- lapply(str.vec, function(v) code.rev[[v]])
+
+		paste0(c(morse, ''), collapse=".")
+	})
+
+	morse <- setClass("morse",
+		representation(stack = "character"),
+		prototype(stack = c())
+	)
+
+	setGeneric('out', function(obj) standardGeneric('out'))
+
+	setMethod('out', 'morse', function(obj)
+		cat(code[[paste0(rev(obj@stack), collapse="")]])
+	)
+
+	setMethod('-', 'morse', function (e1) {
+		e1@stack <- c(e1@stack, '-')
+		e1
+	});
+
+	setMethod('+', 'morse', function (e1) {
+		e1@stack <- c(e1@stack, '+')
+		e1
+	});
+
+	setMethod('+', signature=c('morse', 'morse'), function (e1, e2) {
+		out(e1)
+		+e2
+	});
+
+	setMethod('-', signature=c('morse', 'morse'), function (e1, e2) {
+		out(e1)
+		-e2
+	});
+
+
+	morse()
+}
+
+. <- init()
+
+# cat(toMorseStr("What hath God wrought?"))
+
+out(+--.++++.+-.-.-+++-.++++.+-.-.++++.-+++-.--+.---.-++.-+++-.+--.+-+.---.++-.--+.++++.-.++--++.)
+
+
+
+
